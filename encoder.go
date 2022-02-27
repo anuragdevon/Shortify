79903ebc169b04852a0727858706b4a98c61695a
anuragdevon <msm19b021@iiitdm.ac.in>
2022-02-27 10:27:23 +0530

    cleanup

---
+++ encoder.go
@@ -0,0 +1,54 @@
+package base62
+
+import (
+	"errors"
+	"math"
+	"strings"
+)
+
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	length   = uint64(len(alphabet))
+)
+
+func Encode(number uint64) string {
+	var encodedBuilder strings.Builder
+	encodedBuilder.Grow(11)
+
+	for ; number > 0; number = number / length {
+		encodedBuilder.WriteByte(alphabet[number%length])
+	}
+
+	return encodedBuilder.String()
+}
+
+func Decode(encoded string) (uint64, error) {
+	var number uint64
+
+	for i, symbol := range encoded {
+		alphabeticPosition := strings.IndexRune(alphabet, symbol)
+
+		if alphabeticPosition == -1 {
+			return uint64(alphabeticPosition), errors.New("Invalid Character: " + string(symbol))
+		}
+		number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
+	}
+
+	return number, nil
+}
+
+// Allowed characters in URL-safe Base62 encoding
+// a-z A-Z 0-9_.-~
+
+// Storing of information
+// id integer
+// Original String
+// Expiration time
+// visits integer
+
+// Confurigable values of the service
+// Port to listen to
+// The schema for the short links
+//  Domain for the short links
+// DB connections parameters
+// For simplicity will be storing everything in Json format
---
+++ storage.go
@@ -0,0 +1,13 @@
+package storage
+
+import "time"
+
+type Service interface {
+	Save(string, time.Time) (string, error)
+	Load(string) (string, error)
+	LoadInfo(string) (*Item, error)
+	Close() error
+}
+
+type Item struct {
+}
