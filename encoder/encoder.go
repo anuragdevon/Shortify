a0698b382120d815582832d639ee18b1204c9bec
anuragdevon <msm19b021@iiitdm.ac.in>
2022-02-27 10:29:59 +0530

    add encoder for url

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -16,7 +16,7 @@ func Encode(number uint64) string {
 	encodedBuilder.Grow(11)
 
 	for ; number > 0; number = number / length {
-		encodedBuilder.WriteByte(alphabet[number%length])
+		encodedBuilder.WriteByte(alphabet[(number % length)])
 	}
 
 	return encodedBuilder.String()
@@ -29,26 +29,10 @@ func Decode(encoded string) (uint64, error) {
 		alphabeticPosition := strings.IndexRune(alphabet, symbol)
 
 		if alphabeticPosition == -1 {
-			return uint64(alphabeticPosition), errors.New("Invalid Character: " + string(symbol))
+			return uint64(alphabeticPosition), errors.New("invalid character: " + string(symbol))
 		}
 		number += uint64(alphabeticPosition) * uint64(math.Pow(float64(length), float64(i)))
 	}
 
 	return number, nil
 }
-
-// Allowed characters in URL-safe Base62 encoding
-// a-z A-Z 0-9_.-~
-
-// Storing of information
-// id integer
-// Original String
-// Expiration time
-// visits integer
-
-// Confurigable values of the service
-// Port to listen to
-// The schema for the short links
-//  Domain for the short links
-// DB connections parameters
-// For simplicity will be storing everything in Json format
